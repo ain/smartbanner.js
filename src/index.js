064984ece809124875551b882c16785feaa7f8ab
Ain Tohvri <ain@flashbit.net>
2016-06-01 02:31:07 +0200

    Fix index page typo [ci skip]

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import SmartBanner from './smartbanner.js';
 let parser = new OptionParser;
 let smartbanner = new SmartBanner(parser.options);
 
-document.write('SmartBanner initilised with: ' + JSON.stringify(smartbanner.options));
+document.write('SmartBanner initialised with: ' + JSON.stringify(smartbanner.options));
