1efebde4f2b9c354b8c882d36fc98f8f022480dc
Ain Tohvri <ain@flashbit.net>
2016-06-12 20:24:52 +0200

    Update detector spec to DRYer state

--- test/spec/detector_spec.js
+++ test/spec/detector_spec.js
@@ -9,15 +9,19 @@ describe('Detector', function() {
 
   describe('os', function() {
 
+    const USER_AGENT_IPHONE = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1';
+    const USER_AGENT_IPAD = 'Mozilla/5.0 (iPad; CPU OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1';
+    const USER_AGENT_IPOD = 'Mozilla/5.0 (iPod touch; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4'
+    const USER_AGENT_ANDROID = 'Mozilla/5.0 (Linux; Android 5.1; XT1039 Build/LPB23.13-17.6; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/50.0.2661.86 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/79.0.0.18.71;]'
+
+    const HTML = `<!doctype html><html><head></head><body></body></html>`;
+
     let platform = null;
-    let template = `<!doctype html><html><head></head><body></body></html>`;
 
     context('when on iPhone', function() {
 
       before(function() {
-        global.window = jsdom.jsdom(template, {
-            userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1'
-          }).defaultView;
+        global.window = jsdom.jsdom(HTML, { userAgent: USER_AGENT_IPHONE }).defaultView;
         platform = Detector.platform();
       });
 
@@ -30,9 +34,7 @@ describe('Detector', function() {
     context('when on iPad', function() {
 
       before(function() {
-        global.window = jsdom.jsdom(template, {
-            userAgent: 'Mozilla/5.0 (iPad; CPU OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1'
-          }).defaultView;
+        global.window = jsdom.jsdom(HTML, { userAgent: USER_AGENT_IPAD }).defaultView;
         platform = Detector.platform();
       });
 
@@ -45,9 +47,7 @@ describe('Detector', function() {
     context('when on iPod', function() {
 
       before(function() {
-        global.window = jsdom.jsdom(template, {
-            userAgent: 'Mozilla/5.0 (iPod touch; CPU iPhone OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4'
-          }).defaultView;
+        global.window = jsdom.jsdom(HTML, { userAgent: USER_AGENT_IPOD }).defaultView;
         platform = Detector.platform();
       });
 
@@ -60,9 +60,7 @@ describe('Detector', function() {
     context('when on Android', function() {
 
       before(function() {
-        global.window = jsdom.jsdom(template, {
-            userAgent: 'Mozilla/5.0 (Linux; Android 5.1; XT1039 Build/LPB23.13-17.6; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/50.0.2661.86 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/79.0.0.18.71;]'
-          }).defaultView;
+        global.window = jsdom.jsdom(HTML, { userAgent: USER_AGENT_ANDROID }).defaultView;
         platform = Detector.platform();
       });
 
