f528684c49d0f3501772ed3f2d2fa5fd26492737
Ain Tohvri <ain@flashbit.net>
2016-06-12 16:29:20 +0200

    Add template and price suffic handling

--- src/smartbanner.js
+++ src/smartbanner.js
@@ -1,10 +1,32 @@
 import OptionParser from './optionparser.js';
+import Detector from './detector.js';
 
 export default class SmartBanner {
 
   constructor() {
     let parser = new OptionParser();
     this.options = parser.parse();
+    this.platform = Detector.platform();
+  }
+
+  get priceSuffix() {
+    if (this.platform === 'ios') {
+      return this.options.priceSuffixApple;
+    } else if (this.platform == 'android') {
+      return this.options.priceSuffixAndroid;
+    }
+  }
+
+  get template() {
+    return `<div class="smartbanner smartbanner--${this.platform}">
+      <div class="smartbanner__icon"></div>
+      <div class="smartbanner__info">
+        <div class="smartbanner__info__title">${this.options.title}</div>
+        <div class="smartbanner__info__author">${this.options.author}</div>
+        <div class="smartbanner__info__price">${this.options.price}${this.priceSuffix}</div>
+        <div class="smartbanner__button">${this.options.button}</div>
+      </div>
+    </div>`;
   }
 
   publish() {
--- test/spec/smartbanner_spec.js
+++ test/spec/smartbanner_spec.js
@@ -7,9 +7,23 @@ import SmartBanner from '../../src/smartbanner.js';
 
 describe('SmartBanner', function() {
 
-  describe('publish', function() {
+  let smartbanner = null;
+  let template = `<!doctype html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="smartbanner:title" content="Smart Application">
+      <meta name="smartbanner:author" content="SmartBanner Contributors">
+      <meta name="smartbanner:price" content="FREE">
+      <meta name="smartbanner:price-suffix-apple" content=" - On the App Store">
+      <meta name="smartbanner:price-suffix-google" content=" - In Google Play">
+      <meta name="smartbanner:button" content="VIEW">
+    </head>
+    <body>
+    </body>
+  </html>`;
 
-    let smartbanner = null;
+  describe('publish', function() {
 
     context('without options', function() {
 
@@ -19,6 +33,44 @@ describe('SmartBanner', function() {
 
       it('expected to throw error', function() {
         expect(() => smartbanner.publish()).to.throw('No options detected. Please consult documentation.');
+      });
+
+    });
+
+  });
+
+  describe('template', function() {
+
+    context('when on iPhone', function() {
+
+      before(function() {
+        global.window = jsdom.jsdom(template, {
+          userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1'
+        }).defaultView;
+        global.document = window.document;
+        smartbanner = new SmartBanner();
+      });
+
+      it('expected to work against iOS platform', function() {
+        expect(smartbanner.platform).to.eql('ios');
+      });
+
+      it('expected to have iOS price suffix', function() {
+        expect(smartbanner.priceSuffix).to.eql(' - On the App Store');
+      });
+
+      it('expected to return iOS template', function() {
+        // Incorrect indentation to match template
+        let expectation = `<div class="smartbanner smartbanner--ios">
+      <div class="smartbanner__icon"></div>
+      <div class="smartbanner__info">
+        <div class="smartbanner__info__title">Smart Application</div>
+        <div class="smartbanner__info__author">SmartBanner Contributors</div>
+        <div class="smartbanner__info__price">FREE - On the App Store</div>
+        <div class="smartbanner__button">VIEW</div>
+      </div>
+    </div>`;
+        expect(smartbanner.template).to.equal(expectation);
       });
 
     });
