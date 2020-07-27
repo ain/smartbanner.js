a88984d7ed6b4f0cdb3552462dff7cb713f6fea2
Ain Tohvri <ain@flashbit.net>
2020-07-27 23:10:44 +0200

    Remove new container logic (malfunctions)

--- dist/smartbanner.js
+++ dist/smartbanner.js
@@ -1,5 +1,5 @@
 /*!
- * smartbanner.js v2.0.0-beta.1 <https://github.com/ain/smartbanner.js>
+ * smartbanner.js v2.0.0-beta.2 <https://github.com/ain/smartbanner.js>
  * Copyright © 2020 Ain Tohvri, contributors. Licensed under GPL-3.0.
  */
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
