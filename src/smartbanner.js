0849ba1a91ede01f49f3e21825189fc16d47e8a6
Henning Kuehl <henning.kuehl@simpleclub.com>
2023-11-29 06:15:18 +0100

    improve for backwards compatibility

--- src/smartbanner.js
+++ src/smartbanner.js
@@ -142,9 +142,9 @@ export default class SmartBanner {
   get buttonLabel() {
     let buttonLabel = this.options.button;
     if (this.platform === 'android') {
-      buttonLabel = this.options.buttonGoogle;
+      buttonLabel = this.options.buttonGoogle || this.options.button || DEFAULT_BUTTON_LABEL;
     } else if (this.platform === 'ios') {
-      buttonLabel = this.options.buttonApple;
+      buttonLabel = this.options.buttonApple || this.options.button || DEFAULT_BUTTON_LABEL;
     }
 
     return buttonLabel || DEFAULT_BUTTON_LABEL;
