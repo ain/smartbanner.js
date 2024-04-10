5f0645b8933ba7577b4bbc366637c40b1911485d
Ain Tohvri <ain@flashbit.net>
2024-04-10 23:13:54 +0200

    Update ESLint configuration to new format

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    "rules": {
+      "indent": [
+        "error",
+        2
+      ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ],
+      "eqeqeq": [
+        "error",
+        "always"
+      ],
+      "no-multiple-empty-lines": [
+        "error",
+        {"max": 1}
+      ]
+    }
+  }
+];
