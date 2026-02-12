d77ac3017b13c040863a4722201df538640419ba
Ain Tohvri <ain@flashbit.net>
2026-02-13 00:46:03 +0100

    Replace global reference with window

--- src/detector.js
+++ src/detector.js
@@ -18,7 +18,7 @@ export default class Detector {
   }
 
   static jQueryMobilePage() {
-    return typeof global.$ !== 'undefined' && global.$.mobile !== 'undefined' && document.querySelector('.ui-page') !== null;
+    return typeof window.$ !== 'undefined' && window.$.mobile !== 'undefined' && document.querySelector('.ui-page') !== null;
   }
 
   static wrapperElement() {
